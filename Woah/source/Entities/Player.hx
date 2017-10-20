03f2a5267384e6810326afce48db83eab5273a56
Gaspar Nuñez <gaspacho09@hotmail.com>
2017-10-20 18:09:12 -0300

    Player states

--- Woah/source/Entities/Player.hx
+++ Woah/source/Entities/Player.hx
@@ -1,4 +1,4 @@
-package Entities;
+package entities;
 
 import flixel.FlxSprite;
 import flixel.system.FlxAssets.FlxGraphicAsset;
@@ -6,7 +6,8 @@ import flixel.system.FlxAssets.FlxGraphicAsset;
 import flixel.FlxG;
 import flixel.FlxObject;
 
-enum estados{
+enum Estados
+{
 	IDLE;
 	JUMP;
 	RUN;
@@ -14,32 +15,107 @@ enum estados{
 	ATTACK;
 }
 
-actionState:estados;
+
  
 class Player extends FlxSprite 
 {
-
+	public var actionState(get, null):Estados;
 	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
 	{
 		super(X, Y, SimpleGraphic);
+		actionState = IDLE;
 	}
 	
 	override public function update(elapsed:Float):Void 
 	{
+		checkEstados();
+		acceleration.y = 750;
 		super.update(elapsed);
-		checkEstados(
+	}
+	
+	private function checkEstados():Void
+	{
+		switch(actionState)
+		{
+			//IDLE
+			case Estados.IDLE:
+			movimiento();
+			salto();
+			if (velocity.y < 0)
+			{
+				actionState = Estados.JUMP;
+			}
+			else if (velocity.y > 0)
+			{
+				actionState = Estados.FALLING;
+			}
+			else if (velocity.x != 0)
+			{
+				actionState = Estados.RUN;
+			}
+			else if (velocity.x == 0)
+			{
+				actionState = Estados.IDLE;
+			}
+			//RUN
+			case Estados.RUN:
+			movimiento();
+			salto();
+			if (velocity.y != 0)
+			{
+				actionState = Estados.JUMP;
+			}
+			else if (velocity.x == 0)
+			{
+				actionState = Estados.IDLE;
+			}
+			case Estados.JUMP:
+			if (velocity.y == 0)
+			{
+				if (velocity.x == 0)
+				{
+					actionState = Estados.IDLE;
+				}
+				else
+				actionState = Estados.RUN;
+			}
+			case Estados.FALLING:
+			if (velocity.y == 0)
+			{
+				if (velocity.x == 0)
+				{
+					actionState = Estados.IDLE;
+				}
+				else
+				actionState = Estados.RUN;
+			}
+			case Estados.ATTACK:
+		}
 	}
 	
 	private function movimiento():Void
 	{
 	velocity.x = 0;
 	if (FlxG.keys.pressed.RIGHT)
-	{
-		velocity.x += 300; 
-	}
+		{
+		velocity.x += 150; 
+		}
 	if (FlxG.keys.pressed.LEFT)
-	{
-		velocty.x -= 300;
+		{
+		velocity.x -= 150;
+		}
 	}
+	
+	private function salto():Void
+	{
+		if (FlxG.keys.justPressed.SPACE)
+		{
+			velocity.y -= 350;
+		}
+	}
+	
+	function get_actionState():Estados 
+	{
+		return actionState;
 	}
 }
--- Woah/source/PlayState.hx
+++ Woah/source/PlayState.hx
@@ -1,6 +1,6 @@
 package;
 
-import Entities.Player;
+import entities.Player;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.FlxG;
@@ -9,17 +9,31 @@ import flixel.text.FlxText;
 class PlayState extends FlxState
 
 {
-	private var player:Player
+	private var player:Player;
+	private var plati:FlxSprite;
+	private var testito:FlxText;
 	override public function create():Void
 	{
-		player = new Player;
-		player.makeGraphic(32,32,FlxG.
+		player = new Player(350,300);
+		player.makeGraphic(32, 32, 0xFF000000);
+		FlxG.camera.bgColor = 0xFFCC4466;
+		
+		testito = new FlxText(16, 16, 0, "", 24);
+		plati = new FlxSprite(100, 350);
+		plati.makeGraphic(440, 32);
+		plati.immovable = true;
+		
 		super.create();
-		add player;
+		add(player);
+		add(plati);
+		add(testito);
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+		
+		FlxG.collide(plati, player);
+		testito.text = player.actionState.getName();
 	}
 }
