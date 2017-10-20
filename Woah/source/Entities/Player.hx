b61e35648b43ba65c0b28e365318281b82c8ce34
Gaspar Nuñez <gaspacho09@hotmail.com>
2017-10-20 16:26:53 -0300

    Player++

--- Woah/source/Entities/Player.hx
+++ Woah/source/Entities/Player.hx
@@ -2,18 +2,44 @@ package Entities;
 
 import flixel.FlxSprite;
 import flixel.system.FlxAssets.FlxGraphicAsset;
+import flixel.system.FlxAssets.FlxGraphicAsset;
+import flixel.FlxG;
+import flixel.FlxObject;
 
-/**
- * ...
- * @author ...
- */
+enum estados{
+	IDLE;
+	JUMP;
+	RUN;
+	FALLING;
+	ATTACK;
+}
+
+actionState:estados;
+ 
 class Player extends FlxSprite 
 {
 
 	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
 	{
 		super(X, Y, SimpleGraphic);
-		
 	}
 	
+	override public function update(elapsed:Float):Void 
+	{
+		super.update(elapsed);
+		checkEstados(
+	}
+	
+	private function movimiento():Void
+	{
+	velocity.x = 0;
+	if (FlxG.keys.pressed.RIGHT)
+	{
+		velocity.x += 300; 
+	}
+	if (FlxG.keys.pressed.LEFT)
+	{
+		velocty.x -= 300;
+	}
+	}
 }
