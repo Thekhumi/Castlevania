fd3a3f294b55225234eb68e7cb5c62f38a874b1b
Gaspar Nuñez <gaspacho09@hotmail.com>
2017-10-20 15:24:17 -0300

    Player

--- Woah/source/PlayState.hx
+++ Woah/source/PlayState.hx
@@ -1,12 +1,21 @@
 package;
 
+import Entities.Player;
+import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.FlxG;
+import flixel.text.FlxText;
 
 class PlayState extends FlxState
+
 {
+	private var player:Player
 	override public function create():Void
 	{
+		player = new Player;
+		player.makeGraphic(32,32,FlxG.
 		super.create();
+		add player;
 	}
 
 	override public function update(elapsed:Float):Void
